1a6dda49ddd809d9adeb005a0e92643231f78cef
gayaldassanayake <g.c.dassanayake@gmail.com>
2022-02-07 04:59:18 +0530

    Add a todo list for the parser

--- WinZigC/src/main/java/com/cs4542/compiler/parser/Parser.java
+++ WinZigC/src/main/java/com/cs4542/compiler/parser/Parser.java
@@ -1,4 +1,14 @@
 package com.cs4542.compiler.parser;
 
 public class Parser {
+    /**
+     * TODO
+     * 1. Define build tree function
+     * 2. Define read_token function
+     * 3. Complete procedure for grammar rules of one non terminal
+     * 4. Complete all the procedures
+     * 5. Print output in the desired manner
+     * 6. Test for all 15 examples
+     * 7. (Optional) create automated github test action
+     */
 }

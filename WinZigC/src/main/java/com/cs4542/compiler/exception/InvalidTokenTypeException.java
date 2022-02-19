cb6426f0207835bbbec89c96a374c7f2450ff241
gayaldassanayake <g.c.dassanayake@gmail.com>
2022-02-19 09:58:59 +0530

    Reduce package depth

--- WinZigC/src/main/java/com/cs4542/compiler/exception/InvalidTokenTypeException.java
+++
@@ -1,9 +0,0 @@
-package com.cs4542.compiler.exception;
-
-import com.cs4542.compiler.token.tokentype.TokenType;
-
-public class InvalidTokenTypeException extends Exception {
-    public InvalidTokenTypeException(TokenType type) {
-        super("Invalid " + type + " token for the token");
-    }
-}
--- WinZigC/src/main/java/com/cs4542/compiler/exception/OutOfOrderTokenException.java
+++
@@ -1,9 +0,0 @@
-package com.cs4542.compiler.exception;
-
-import com.cs4542.compiler.token.ScannerToken;
-
-public class OutOfOrderTokenException extends Exception {
-    public OutOfOrderTokenException(ScannerToken token) {
-        super("Token "+ token.getValue() +" found out of order");
-    }
-}
